c26acd8ee727f9bf2e2124ff377957cbc833d236
alan00035 <alan00035@hotmail.com>
2021-04-11 20:12:03 +1000

    clear k8s folder

--- hackerRank/BigDecimal.java
+++ hackerRank/BigDecimal.java
@@ -1,24 +1,36 @@
 import java.math.BigDecimal;
 
-class BigDecimal{
+class test{
 
     public static void main(String[] args) {
         String[] s ={"9","-100","50","0","56.6","90","0.12",".12","02.34","000.000"};
         String temp=null;
 
-        for(int i =0; i<s.length-1; i++){
+        for(int i =0; i<s.length-2; i++){
           for(int j=i+1; j<s.length; j++){
             BigDecimal b1 = new BigDecimal(s[i]);
             BigDecimal b2 = new BigDecimal(s[j]);
-              if ( b1.compareTo(b2) < 0 ){
+              if ( b1.compareTo(b2) < 0  ){
                 temp = s[i];
                 s[i] = s[j];
                 s[j] = temp;
-
               }
+              
           }
             
         }
+
+      //   for (int i = 0; i < s.length-2; i++) {
+      //     for (int j = 1; j < (10-i); j++) {
+      //         BigDecimal sj = new BigDecimal(s[j]);
+      //         BigDecimal sj1 = new BigDecimal(s[j-1]);
+      //         if(sj.compareTo(sj1) == 1){
+      //              temp = s[j];
+      //             s[j] = s[j-1];
+      //             s[j-1] = temp;
+      //         }
+      //     }       
+      // }
 
         for(String st: s){
             System.out.println(st);
--- hackerRank/practise.java
+++ hackerRank/practise.java
@@ -1,34 +1,24 @@
-import java.lang.reflect.Array;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+
+import java.math.BigInteger;
 
 import jdk.nashorn.internal.parser.Token;
 
 public class practise {
    public static void main(String[] args){
-      int n =1;
-      int k=10;
 
-      int c = n & k;
 
-      System.out.println(c);
+      BigInteger bigInt = BigInteger.valueOf(Integer.parseInt("13"));
 
-      for(int A =1; A<=n; A++){
-         if( A)
+      if( bigInt.isProbablePrime(10) == false){
+         System.out.println("not prime");
       }
-      
+      else {
+         System.out.println("prime");
+      }
+
+   
+
 
 
        
