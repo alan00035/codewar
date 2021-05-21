14195f7eb50dad1492cf8e0584e94f0a3a51c115
alan00035 <alan00035@hotmail.com>
2021-05-21 20:03:57 +1000

    update

--- hackerRank/practise.java
+++ hackerRank/practise.java
@@ -2,13 +2,14 @@
 
 import java.math.BigInteger;
 
+import jdk.nashorn.internal.parser.Scanner;
 import jdk.nashorn.internal.parser.Token;
 
 public class practise {
    public static void main(String[] args){
 
 
-      BigInteger bigInt = BigInteger.valueOf(Integer.parseInt("13"));
+      BigInteger bigInt = new BigInteger("2367495770217142995264827948666809233066409497699870112003149352380375124855230068487109373226251983");
 
       if( bigInt.isProbablePrime(10) == false){
          System.out.println("not prime");
