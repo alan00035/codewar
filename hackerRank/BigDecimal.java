7ee6044ce7596ff17dbf0d6a1303a7aa43810ac8
alan00035 <alan00035@hotmail.com>
2021-06-30 21:55:15 +1000

    update

--- hackerRank/BigDecimal.java
+++ hackerRank/BigDecimal.java
@@ -1,3 +1,4 @@
+
 import java.math.BigDecimal;
 
 class test{
---
+++ hackerRank/subarray.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class subarray {
+
+    public static void main(String[] args) {
+        
+    
+    int[] arr = {1,-2,4,-5,1};
+
+    int lengh = 5;
+
+    for(int i =0; i<lengh; i++){
+
+    }
+    
+    }
+
+}
